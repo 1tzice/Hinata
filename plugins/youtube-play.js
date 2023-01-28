3647a816eff1267fd511b3264fe67f331b02d806
Wudysoft <116274228+wudysoft@users.noreply.github.com>
2023-01-28 09:57:01 +0800

    Add files via upload

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,16 +1,32 @@
-import { youtubeSearch } from '@bochilteam/scraper'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-await conn.sendMessage(m.chat, {
-          react: {
+import {
+    youtubeSearch
+} from '@bochilteam/scraper'
+let handler = async (m, {
+    conn,
+    command,
+    text,
+    usedPrefix
+}) => {
+    await conn.sendMessage(m.chat, {
+        react: {
             text: '⏳',
             key: m.key,
-          }})
-  if (!text) throw `Use example ${usedPrefix}${command} Dj Gama Naufal`
-  let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-  const url = 'https://www.youtube.com/watch?v=' + videoId
-  let caption = `
+        }
+    })
+    if (!text) throw `Use example ${usedPrefix}${command} Dj Gama Naufal`
+    let vid = (await youtubeSearch(text)).video[0]
+    if (!vid) throw 'Video/Audio Tidak ditemukan'
+    let {
+        title,
+        description,
+        thumbnail,
+        videoId,
+        durationH,
+        viewH,
+        publishedTime
+    } = vid
+    const url = 'https://www.youtube.com/watch?v=' + videoId
+    let caption = `
 *${htki} PLAY ${htka}*
 
 ${htjava} *Title:* ${title}
@@ -21,15 +37,15 @@ ${htjava} *Title:* ${title}
 🔗 *Url:* ${url}
 📔 *Description:* ${description}
   `.trim()
-  let listSections = []
-	
-	listSections.push(['[ PILIH OPSI YANG KAMU MAU ]', [
-    ['🎶 Audio', usedPrefix + 'yta ' + url + ' yes'],
-    ['🎥 Video', usedPrefix + 'ytv ' + url + ' yes'],
-    ['🔎 Youtube Search', usedPrefix + 'yts ' + url]
-        ]])
-	
-	return conn.sendList(m.chat, '', caption, author, '📣 GO TO YOUTUBE', listSections, m)
+    let listSections = []
+
+    listSections.push(['[ PILIH OPSI YANG KAMU MAU ]', [
+        ['🎶 Audio', usedPrefix + 'yta ' + url + ' yes'],
+        ['🎥 Video', usedPrefix + 'ytv ' + url + ' yes'],
+        ['🔎 Youtube Search', usedPrefix + 'yts ' + url]
+    ]])
+
+    return conn.sendList(m.chat, '', caption, author, '📣 GO TO YOUTUBE', listSections, m)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
